f31c414db5d157100959fb926f576e3a2c62e6e9
robotic7o7 <scientificrohan@gmail.com>
2021-07-06 11:24:48 +0530

    latest commit

--- src/pages/view-assignment/view-assignment.js
+++ src/pages/view-assignment/view-assignment.js
@@ -107,92 +107,173 @@ function ViewAssignment(props) {
 
 
     if (assignment) {
-        return (
-            <>
-            <div className="view-assignment">
-                <div className="teacher-sort-bar">
-                    <button className="submit-button submit-assignment-button" onClick={displayUpload}>Submit Assignment</button>
-                    {/* <div className="query-function-container">
-           <button className="query-button">Sort Asscending</button>
-            <button className="query-button">Sort Descending</button>
-            <button className="query-button">Sort Alphabetically</button>
-           </div> */}
-                </div>
-
-                <div className="assignment-details">
-                    <span className="assignment-details-name">{assignment.assignment_name}</span>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <div>
-                    <span className="question-value">Assignment Image</span>
-                    <img src={`https://fks-assignments.s3.ap-south-1.amazonaws.com/${assignment.file}`} className="assignment-image-view"/>
+        if(assignment.file!=undefined){
+            return (
+                <>
+                <div className="view-assignment">
+                    <div className="teacher-sort-bar">
+                        <button className="submit-button submit-assignment-button" onClick={displayUpload}>Submit Assignment</button>
+                        {/* <div className="query-function-container">
+               <button className="query-button">Sort Asscending</button>
+                <button className="query-button">Sort Descending</button>
+                <button className="query-button">Sort Alphabetically</button>
+               </div> */}
                     </div>
-                    
-                    {assignment.questions.map((item) => {
-                        if (item.question_type == "MCQ") {
-                            return (
-                                <div className="question">
-                                    <span className="question-value">{i + ") " + item.question_text}</span>
-                                    <span className="question-option">1) {item.option1}</span>
-                                    <span className="question-option">2) {item.option2}</span>
-                                    <span className="question-option">3) {item.option3}</span>
-                                    <span className="question-option">4) {item.option4}</span>
-                                    {/* <span className="hidden">{i++}{j = 1}</span> */}
-                                </div>
-                            )
-
-                        }
-                        else if (item.question_type == "LA") {
-                            return (
-                                <div className="question">
-                                    <span className="question-value">{i + ") " + item.question_text}</span>
-                                    <span className="hidden">{i++}</span>
-                                </div>
-                            )
-
-                        }
-                        else if (item.question_type == "SA") {
-                            return (
-                                <div className="question">
-                                    <span className="question-value">{i + ") " + item.question_text}</span>
-                                    <span className="hidden">{i++}</span>
-                                </div>
-                            )
-
-                        }
-                    })}
+    
+                    <div className="assignment-details">
+                        <span className="assignment-details-name">{assignment.assignment_name}</span>
+                        <div className="assignment-image-view-section" id="assignment-image-view-section">
+                        <span className="question-value">Assignment Image</span>
+                        <img src={`https://fks-assignments.s3.ap-south-1.amazonaws.com/${assignment.file}`} className="assignment-image-view"/>
+                        </div>
+                        
+                        {assignment.questions.map((item) => {
+                            if (item.question_type == "MCQ") {
+                                return (
+                                    <div className="question">
+                                        <span className="question-value">{i + ") " + item.question_text}</span>
+                                        <span className="question-option">1) {item.option1}</span>
+                                        <span className="question-option">2) {item.option2}</span>
+                                        <span className="question-option">3) {item.option3}</span>
+                                        <span className="question-option">4) {item.option4}</span>
+                                        {/* <span className="hidden">{i++}{j = 1}</span> */}
+                                    </div>
+                                )
+    
+                            }
+                            else if (item.question_type == "LA") {
+                                return (
+                                    <div className="question">
+                                        <span className="question-value">{i + ") " + item.question_text}</span>
+                                        <span className="hidden">{i++}</span>
+                                    </div>
+                                )
+    
+                            }
+                            else if (item.question_type == "SA") {
+                                return (
+                                    <div className="question">
+                                        <span className="question-value">{i + ") " + item.question_text}</span>
+                                        <span className="hidden">{i++}</span>
+                                    </div>
+                                )
+    
+                            }
+                        })}
+                    </div>
                 </div>
-            </div>
-
-            <div className="upload-assignment-popup" id="assignment-upload">
-             <div className="form-upload-container">
-             <span className="form-title">Upload Assignment</span>
-                <div className="form-field-container">
-                <label className="form-field-label">Upload File</label>
-                <input className="form-field full-width-field" type="file" onChange={e => { uploadFile(e) }} />
-                </div>
-                <button className="submit-button" onClick={hideUpload} >SUBMIT</button>
-                <button className="submit-button button-secondary" onClick={hideUpload}>CLOSE</button>
+    
+                <div className="upload-assignment-popup" id="assignment-upload">
+                 <div className="form-upload-container">
+                 <span className="form-title">Upload Assignment</span>
+                    <div className="form-field-container">
+                    <label className="form-field-label">Upload File</label>
+                    <input className="form-field full-width-field" type="file" onChange={e => { uploadFile(e) }} />
+                    </div>
+                    <button className="submit-button" onClick={hideUpload} >SUBMIT</button>
+                    <button className="submit-button button-secondary" onClick={hideUpload}>CLOSE</button>
+                 </div>
              </div>
-         </div>
+    
+    
+             <div className="notif-component-success"id="notif-success">
+                <label className="notif-component-text">Success!</label>
+                <br/>
+                <label className="notif-component-message">Assignment Submitted.</label>
+                <img src="/icons8-macos-close-60.png" className="notif-closeIcon" onClick={closeNotif}/>
+            </div>
+    
+            <div className="notif-component-failed" id="notif-failed">
+                <label className="notif-component-text">Failed!</label>
+                <br/>
+                <label className="notif-component-message">Error occured, try again.</label>
+                <img src="/icons8-macos-close-60.png" className="notif-closeIcon" onClick={closeNotif}/>
+            </div>
+                </>
+            )
+        }
 
-
-         <div className="notif-component-success"id="notif-success">
-            <label className="notif-component-text">Success!</label>
-            <br/>
-            <label className="notif-component-message">Assignment Submitted.</label>
-            <img src="/icons8-macos-close-60.png" className="notif-closeIcon" onClick={closeNotif}/>
-        </div>
-
-        <div className="notif-component-failed" id="notif-failed">
-            <label className="notif-component-text">Failed!</label>
-            <br/>
-            <label className="notif-component-message">Error occured, try again.</label>
-            <img src="/icons8-macos-close-60.png" className="notif-closeIcon" onClick={closeNotif}/>
-        </div>
-            </>
-        )
+        else{
+            return (
+                <>
+                <div className="view-assignment">
+                    <div className="teacher-sort-bar">
+                        <button className="submit-button submit-assignment-button" onClick={displayUpload}>Submit Assignment</button>
+                        {/* <div className="query-function-container">
+               <button className="query-button">Sort Asscending</button>
+                <button className="query-button">Sort Descending</button>
+                <button className="query-button">Sort Alphabetically</button>
+               </div> */}
+                    </div>
+    
+                    <div className="assignment-details">
+                        <span className="assignment-details-name">{assignment.assignment_name}</span>
+                        
+                        {assignment.questions.map((item) => {
+                            if (item.question_type == "MCQ") {
+                                return (
+                                    <div className="question">
+                                        <span className="question-value">{i + ") " + item.question_text}</span>
+                                        <span className="question-option">1) {item.option1}</span>
+                                        <span className="question-option">2) {item.option2}</span>
+                                        <span className="question-option">3) {item.option3}</span>
+                                        <span className="question-option">4) {item.option4}</span>
+                                        {/* <span className="hidden">{i++}{j = 1}</span> */}
+                                    </div>
+                                )
+    
+                            }
+                            else if (item.question_type == "LA") {
+                                return (
+                                    <div className="question">
+                                        <span className="question-value">{i + ") " + item.question_text}</span>
+                                        <span className="hidden">{i++}</span>
+                                    </div>
+                                )
+    
+                            }
+                            else if (item.question_type == "SA") {
+                                return (
+                                    <div className="question">
+                                        <span className="question-value">{i + ") " + item.question_text}</span>
+                                        <span className="hidden">{i++}</span>
+                                    </div>
+                                )
+    
+                            }
+                        })}
+                    </div>
+                </div>
+    
+                <div className="upload-assignment-popup" id="assignment-upload">
+                 <div className="form-upload-container">
+                 <span className="form-title">Upload Assignment</span>
+                    <div className="form-field-container">
+                    <label className="form-field-label">Upload File</label>
+                    <input className="form-field full-width-field" type="file" onChange={e => { uploadFile(e) }} />
+                    </div>
+                    <button className="submit-button" onClick={hideUpload} >SUBMIT</button>
+                    <button className="submit-button button-secondary" onClick={hideUpload}>CLOSE</button>
+                 </div>
+             </div>
+    
+    
+             <div className="notif-component-success"id="notif-success">
+                <label className="notif-component-text">Success!</label>
+                <br/>
+                <label className="notif-component-message">Assignment Submitted.</label>
+                <img src="/icons8-macos-close-60.png" className="notif-closeIcon" onClick={closeNotif}/>
+            </div>
+    
+            <div className="notif-component-failed" id="notif-failed">
+                <label className="notif-component-text">Failed!</label>
+                <br/>
+                <label className="notif-component-message">Error occured, try again.</label>
+                <img src="/icons8-macos-close-60.png" className="notif-closeIcon" onClick={closeNotif}/>
+            </div>
+                </>
+            )
+        }
     }
 
     else {
